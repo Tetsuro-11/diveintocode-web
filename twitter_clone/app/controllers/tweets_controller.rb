976d3ddeeccc523a4d5da78050a4326f01cd8a3d
Tetsuro-11 <nishiotetsurou@nishiotetsurounoMacBook-puro.local>
2019-09-26 18:24:07 +0900

    ツイート一覧機能の追加

--- twitter_clone/app/controllers/tweets_controller.rb
+++ twitter_clone/app/controllers/tweets_controller.rb
@@ -1,7 +1,18 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.all
   end
 
   def new
+    @tweet = Tweet.new
+  end
+  def create
+    Tweet.create(blog_params)
+    redirect_to new_tweet_path
+  end
+
+  private
+  def blog_params
+    params.require(:tweet).permit(:content)
   end
 end
